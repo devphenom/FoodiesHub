2d41fa344eb4970406fa27b5e03da5aa31b2cd05
devphenom <iphenom01@gmail.com>
2020-11-12 15:48:36 +0100

    Added react-bootstrap package

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
+import { Carousel } from "react-bootstrap";
 
 import { fetchRecipeDetails } from "../Redux/actionCreators";
 import "./Details.css";
@@ -120,7 +121,57 @@ const Details = (props) => {
                   </div>
                 ))}
               </div>
-              .row
+              <div className="row">
+                <div className="col-12">
+                  <h4 className="font-weight-bold">Procedures:</h4>
+                  <div className="shadow rounded p-3">
+                    <Carousel>
+                      <Carousel.Item interval={null}>
+                        <img
+                          className="d-block w-100"
+                          src="holder.js/800x400?text=First slide&bg=373940"
+                          alt="First slide"
+                        />
+                        <Carousel.Caption>
+                          <h3>First slide label</h3>
+                          <p>
+                            Nulla vitae elit libero, a pharetra augue mollis
+                            interdum.
+                          </p>
+                        </Carousel.Caption>
+                      </Carousel.Item>
+                      <Carousel.Item interval={500}>
+                        <img
+                          className="d-block w-100"
+                          src="holder.js/800x400?text=Second slide&bg=282c34"
+                          alt="Third slide"
+                        />
+                        <Carousel.Caption>
+                          <h3>Second slide label</h3>
+                          <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit.
+                          </p>
+                        </Carousel.Caption>
+                      </Carousel.Item>
+                      <Carousel.Item>
+                        <img
+                          className="d-block w-100"
+                          src="holder.js/800x400?text=Third slide&bg=20232a"
+                          alt="Third slide"
+                        />
+                        <Carousel.Caption>
+                          <h3>Third slide label</h3>
+                          <p>
+                            Praesent commodo cursus magna, vel scelerisque nisl
+                            consectetur.
+                          </p>
+                        </Carousel.Caption>
+                      </Carousel.Item>
+                    </Carousel>
+                  </div>
+                </div>
+              </div>
             </div>
             {console.log(fetchedData)}
           </div>
