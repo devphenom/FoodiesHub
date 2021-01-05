78ad62d1cd36ded728e56a847b2c03ae73589787
devphenom <iphenom01@gmail.com>
2021-01-05 21:35:36 +0100

    Installed enzyme

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Provider } from "react-redux";
 // import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
 import { Router } from "@reach/router";
@@ -12,21 +12,24 @@ import NoMatch from "./components/NoMatch";
 
 import "./App.css";
 
+const Home = lazy(() => import("./"));
+
 const App = () => {
   return (
     // <Router>
     <Provider store={store}>
       <div id="main">
         <Router>
-          <Home path="/" />
-          <Search path="/search" />
-          <Details path="/details/:id" />
-          <NoMatch default />
+          <Suspense>
+            <Home path="/" />
+            <Search path="/search" />
+            <Details path="/details/:id" />
+            <NoMatch default />
+          </Suspense>
         </Router>
         {/* <Footer /> */}
       </div>
     </Provider>
-    // </Router>
   );
 };
 
